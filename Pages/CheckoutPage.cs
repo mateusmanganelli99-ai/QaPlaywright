5cd313b47a94e551bcefc8b09c7ad69bd725f522
Mateus Manganelli <mateusmanganelli99@gmail.com>
2026-04-29 10:13:35 -0300

    C# 001

--- Pages/CheckoutPage.cs
+++ Pages/CheckoutPage.cs
@@ -1,4 +1,5 @@
 using Microsoft.Playwright;
+using QaPlaywright.Utils;
 
 namespace QaPlaywright.Pages;
 
@@ -13,31 +14,53 @@ public class CheckoutPage
 
     public async Task PreencherPagamento()
     {
-        if (!_page.Url.Contains("/payment"))
+        var nomeCartao = _page.Locator("input[data-qa='name-on-card']");
+
+        if (!await nomeCartao.IsVisibleAsync())
         {
-            await _page.WaitForURLAsync("**/checkout", new() { Timeout = 15000 });
-
-            var comentario = _page.Locator("textarea[name='message']");
-            if (await comentario.IsVisibleAsync())
-            {
-                await comentario.FillAsync("Pedido criado por teste automatizado.");
-            }
-
-            await _page.Locator("a[href='/payment']").ClickAsync();
+            await IrParaTelaDePagamento();
         }
 
-        await _page.WaitForURLAsync("**/payment", new() { Timeout = 15000 });
+        await nomeCartao.WaitForAsync(new() { State = WaitForSelectorState.Visible, Timeout = 15000 });
 
-        await _page.Locator("input[data-qa='name-on-card']")
-            .WaitForAsync(new() { State = WaitForSelectorState.Visible, Timeout = 15000 });
-
-        await _page.FillAsync("input[data-qa='name-on-card']", "Mateus QA");
+        await nomeCartao.FillAsync("Mateus QA");
         await _page.FillAsync("input[data-qa='card-number']", "4111111111111111");
         await _page.FillAsync("input[data-qa='cvc']", "123");
         await _page.FillAsync("input[data-qa='expiry-month']", "12");
         await _page.FillAsync("input[data-qa='expiry-year']", "2030");
 
         await _page.ClickAsync("button[data-qa='pay-button']");
+    }
+
+    private async Task IrParaTelaDePagamento()
+    {
+        if (!_page.Url.Contains("/checkout"))
+        {
+            await _page.WaitForURLAsync("**/checkout", new() { Timeout = 15000 });
+        }
+
+        var comentario = _page.Locator("textarea[name='message']");
+        if (await comentario.IsVisibleAsync())
+        {
+            await comentario.FillAsync("Pedido criado por teste automatizado.");
+        }
+
+        var placeOrder = _page.Locator("a[href='/payment']").First;
+        await placeOrder.ScrollIntoViewIfNeededAsync();
+        await placeOrder.ClickAsync();
+
+        try
+        {
+            await _page.Locator("input[data-qa='name-on-card']")
+                .WaitForAsync(new() { State = WaitForSelectorState.Visible, Timeout = 7000 });
+        }
+        catch (TimeoutException)
+        {
+            await _page.GotoAsync($"{Config.BaseUrl}/payment", new()
+            {
+                WaitUntil = WaitUntilState.Commit
+            });
+        }
     }
 
     public async Task ValidarCompra()
